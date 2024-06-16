c7bef8bcf8c130238058a91173f7f04961b4b190
sankalp pathak <pathaksankalp04@gmail.com>
2024-06-16 07:21:14 +0530

    Update main.js

--- lesson3/src/main.js
+++ lesson3/src/main.js
@@ -19,17 +19,12 @@ import {
     GLTFLoader
 } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js?module";
 
-// Get a reference to the container element that will hold our scene
-const container = document.querySelector('#scene-container');
 
-// create a scene 
+const container = document.querySelector('#scene-container');
 
 const scene = new Scene();
 
-//get the background color 
 scene.background = new Color('skyblue');
-
-//create a camera 
 
 const fov = 35;
 const aspect = container.clientWidth / container.clientHeight;
@@ -38,23 +33,21 @@ const far = 100;
 
 const camera = new PerspectiveCamera(fov , aspect, near, far);
 
-//default obj position is at 0,0,0
-// move camera back so we can see the scene 
 camera.position.set(0,0,10);
 
-//create a geometry 
 const geometry = new BoxBufferGeometry(2,2,2);
 
-// creating a default material 
 const material = new MeshBasicMaterial();
 
-// creating a  mesh and putting material and geometry in it 
 const cube = new Mesh(geometry , material);
 
-//add mest to scene 
 scene.add(cube);
 
-//create  a renderer 
 const renderer = new WEBGLRenderer();
 
-//
+renderer.setSize(container.clientWidth, container.clientHeight);
+
+renderer.setPixelRatio(window.devicePixelRatio);
+
+container.append(renderer.domElement);
+
