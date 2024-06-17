bf3eb705a791dc03a8e6069f0ceffac097fc35b3
sankalp pathak <pathaksankalp04@gmail.com>
2024-06-17 19:08:39 +0530

    now you can even see it

--- lesson1/script.js
+++ lesson1/script.js
@@ -31,3 +31,5 @@ renderer.setSize(sizes.width, sizes.height)
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+renderer.render (scene, camera)
