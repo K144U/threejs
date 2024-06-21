90178b703761bc20265a9bb517ebca16d0536c57
sankalp pathak <pathaksankalp04@gmail.com>
2024-06-21 11:38:23 +0530

    minecraft block

--- lesson8/src/script.js
+++ lesson8/src/script.js
@@ -15,7 +15,7 @@ const loadingManager = new THREE.LoadingManager()
 
 
 const textureLoader = new THREE.TextureLoader()
-const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const colorTexture = textureLoader.load('/textures/minecraft.png')
 colorTexture.colorSpace = THREE.SRGBColorSpace
 const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
 const heightTexture = textureLoader.load('/textures/door/height.jpg')
@@ -33,9 +33,13 @@ const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 // colorTexture.offset.x = 0.5
 // colorTexture.offset.y = 0.5
 
-colorTexture.rotation = Math.PI * 0.25
-colorTexture.center.x = 0.5
-colorTexture.center.y = 0.5
+// colorTexture.rotation = Math.PI * 0.25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+colorTexture.generateMipmaps = false
+colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter
+
 
 // image.src = '/textures/door/color.jpg'
 
