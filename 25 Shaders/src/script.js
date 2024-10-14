cbfdf635efd2044ed804bdfa1c2aa91041df90c8
sankalp pathak <pathaksankalp04@gmail.com>
2024-10-14 18:02:38 +0530

    openGL shader language

--- 25 Shaders/src/script.js
+++ 25 Shaders/src/script.js
@@ -29,6 +29,17 @@ const textureLoader = new THREE.TextureLoader()
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
+const count = geometry.attributes.position.count
+const randoms = new Float32Array(count)
+
+for (let i = 0 ; i < count ; i++){
+    randoms[i] = Math.random()
+}
+
+geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
+
+console.log(geometry)
+
 //raw shader material 
 const material = new THREE.RawShaderMaterial({
     vertexShader: testVertexShader,
--- 25 Shaders/src/shaders/test/vertex.glsl
+++ 25 Shaders/src/shaders/test/vertex.glsl
@@ -3,7 +3,22 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
 attribute vec3 position;
+attribute float aRandom;
 
-void main(){
-gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+
+//varying float vRandom;
+
+
+void main()
+{
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    
+    gl_Position = projectedPosition;
+
+
+
+  //  vRandom = aRandom;
 }
